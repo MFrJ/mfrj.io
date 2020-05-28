06a96731a5f61591f046c62571e741a34fc3d5c3
MFrJ <marcelofernaj@gmail.com>
2020-05-27 22:33:47 -0300

    added basic layout

---
+++ src/components/FullBackground.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import styled from 'styled-components'
+
+import BackgroundImage from 'gatsby-background-image'
+import { StyledFullScreenWrapper } from './SharedStyledComponents'
+
+/**
+ * In this functional component a fullscreen <BackgroundImage />  is created.
+ * @param className   string    className(s) from styled-components.
+ * @param children    nodes     Child-components.
+ * @return {*}
+ * @constructor
+ */
+const FullBackground = ({ className, children }) => {
+  const { desktop } = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(relativePath: { eq: "brown-and-black.jpg" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 3344) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `
+  )
+
+  // Single Image Data
+  const imageData = desktop.childImageSharp.fluid
+
+  return (
+    <StyledFullScreenWrapper>
+      <BackgroundImage
+        Tag="section"
+        className={className}
+        fluid={imageData}
+        backgroundColor={`#040e18`}
+        title="Fullscreen Background"
+        id="fullscreenbg"
+        role="img"
+        aria-label="Fullscreen Background"
+        preserveStackingContext={true}
+      >
+        {children}
+      </BackgroundImage>
+    </StyledFullScreenWrapper>
+  )
+}
+
+//TODO Pass Style as props
+const StyledFullBackground = styled(FullBackground)`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.5;
+  &:hover {
+    opacity: 0.75;
+  }
+
+`
+
+export default StyledFullBackground
--- src/components/header.js
+++ src/components/header.js
@@ -5,28 +5,50 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: `#1e1a1d`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `0.45rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#e7ecef`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
+      <h5 style={{ textAlign: "right"}}>
+        <Link
+          to="/about"
+          style={{
+            color: `#e7ecef`,
+            textDecoration: `none`,
+          }}
+        >
+          {" "}
+          About
+        </Link>
+
+        <Link
+          to="/projects"
+          style={{
+            color: `#e7ecef`,
+            textDecoration: `none`,
+            margin: `2em`
+          }}
+        >
+          Projects
+        </Link>
+      </h5>
     </div>
   </header>
 )
